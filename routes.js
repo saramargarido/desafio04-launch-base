3ff63afdeeb83f7425630768ef9b91bcbff41276
saramargarido <saramargarido@gmail.com>
2020-07-29 14:54:57 -0300

    Challenge 4-3, forms, routes and validation

--- routes.js
+++ routes.js
@@ -1,13 +1,21 @@
 const express = require('express')
+const { Router } = require('express')
 const routes = express.Router()
+const teachers = require('./teachers')
 
 routes.get('/', function (req, res) {
-    return res.redirect("instructors")
+    return res.redirect("teachers")
 })
 
-routes.get('/instructors', function (req, res) {
+routes.get('/teachers', function (req, res) {
     return res.render("teacher/index")
 })
+
+routes.get('/teachers/create', function (req, res) {
+    return res.render("teacher/create")
+})
+
+routes.post('/teachers', teachers.post)
 
 routes.get('/members', function (req, res) {
     return res.send("members")
